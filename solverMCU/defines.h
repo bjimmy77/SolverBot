f598f6d7e45899820fbd3f44ed324b0ca1ba5153
bjimmy77 <bryantthesmith@yahoo.com>
2018-07-01 15:48:39 -0700

    Create new framework with the correct libraries. Added a file for defines

---
+++ solverMCU/defines.h
@@ -0,0 +1,49 @@
+//There are 11 Servos in the project/
+//North (top)
+#define NORTHGRIP 0
+#define NORTHGRIPMIN 100
+#define NORTHGRIPMAX 600
+#define NORTHEXT 1
+#define NORTHEXTMIN 100
+#define NORTHEXTMAX 600
+//South (bottom)
+#define SOUTHGRIP 2
+#define SOUTHGRIPMIN 100
+#define SOUTHGRIPMAX 600
+#define SOUTHEXT 3
+#define SOUTHEXTMIN 100
+#define SOUTHEXTMAX 600
+//East (Right)
+#define EASTGRIP 4
+#define EASTGRIPMIN 100
+#define EASTGRIPMAX 600
+#define EASTEXT 5
+#define EASTEXTMIN 100
+#define EASTEXTMAX 600
+//West (left)
+#define WESTGRIP 6
+#define WESTGRIPMIN 100
+#define WESTGRIPMAX 600
+#define WESTEXT 7
+#define WESTEXTMIN 100
+#define WESTEXTMAX 600
+//Scanner
+#define SCANEXT 8
+#define SCANEXTMIN 100
+#define SCANEXTMAX 600
+#define SCANLEFT 9
+#define SCANLEFTMIN 100
+#define SCANLEFTMAX 600
+#define SCANRIGHT 10
+#define SCANRIGHTMIN 100
+#define SCANRIGHTMAX 600
+
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
