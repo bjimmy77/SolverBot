ec610752f5f489552fcba74664ffe59aef42d9f6
bjimmy77 <bryantthesmith@yahoo.com>
2018-07-15 15:03:02 -0700

    Added unified defines

--- solverMCU/solverMCU.ino
+++ solverMCU/solverMCU.ino
@@ -14,20 +14,28 @@
   //https://github.com/MajenkoLibraries/ChangeNotification
 #include <ChangeNotification.h> 
   //Graphics Library for small .96" OLED
-#include <SPI.h>
+#include <SPI.h> //needs SPI.h for SPI
 #include "Ucglib.h"
   //I2C PWM library for Servo Controll
-#include <Wire.h>
+#include <Wire.h> //needs Wire.h for I2C
 #include <Adafruit_PWMServoDriver.h>
-
 
 //Call libraries
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 Ucglib_SSD1331_18x96x64_UNIVISION_HWSPI ucg(/*cd=*/ 9, /*cs=*/ 4, /*reset=*/ 8);
 
 //Definitions
-#include defines.h
+#include "defines.h"
 
+//Variables
+//Encoder
+volatile int rotaryNumber = 0;              // Encoder value, print it when it changes value,
+                                            // used in loop and both interrupt routines
+volatile boolean halfleft = false;          // Used in both interrupt routines
+volatile boolean halfright = false;         // Used in both interrupt routines
+
+int Button = 3;            // Rotary Encoder shaft switch
+int oldnumber = rotaryNumber;
 
 void setup(void) {
   delay(300);
@@ -54,9 +62,57 @@ void setup(void) {
   pwm.setPWM(SCANEXT, 0, 250);
   pwm.setPWM(SCANLEFT, 0, 250);
   pwm.setPWM(SCANRIGHT, 0, 250);
+  //Encoder
+  pinMode(Button, INPUT);          // with external pull-up and cap
+  attachInterrupt(CN_4, isr_A, FALLING);      // Call isr_2 when digital A0 goes LOW
+  attachInterrupt(CN_6, isr_B, FALLING);      // Call isr_3 when digital A1 goes LOW
+
 }
 
 void loop() {
-  
+  if(rotaryNumber != oldnumber){                  // Change in value ?
+    Serial.println(rotaryNumber);                 // Yes, print it (or whatever)
+    oldnumber = rotaryNumber;
+  }
+  if(digitalRead(Button) == LOW && rotaryNumber != 0) {        // Only process 1st reset push
+    rotaryNumber = 0;
+    oldnumber = 0;
+    Serial.println("Reset...");
+  }  
 }
 
+//Encoder ISRs
+void isr_A(){                                           // A went LOW
+  delay(3);                                             // Debounce time
+                                                        // Trade off bounce vs missed counts
+  int bits = PORTB;                                     // Atomic read of encoder inputs
+  int LSB = (bits >> 2) & 0x01;
+  int MSB = (bits >> 4) & 0x01;
+  
+  if(LSB == LOW){                                 // A still LOW ?
+    if(MSB == HIGH && halfright == false){        // -->
+      halfright = true;                                 // One half click clockwise
+    } 
+    if(MSB == LOW && halfleft == true){           // <--
+      halfleft = false;                                 // One whole click counter-
+      rotaryNumber--;                                         // clockwise
+    }
+  }
+}
+void isr_B(){                                           // B went LOW
+  delay(3);                                             // Debounce time
+                                                        // Trade off bounce vs missed counts
+  int bits = PORTB;                                     // Atomic read of encoder inputs
+  int LSB = (bits >> 2) & 0x01;
+  int MSB = (bits >> 4) & 0x01;
+  
+  if(MSB == LOW){                                 // A still LOW ?
+    if(LSB == HIGH && halfleft == false){         // <--
+      halfleft = true;                                  // One half  click counter-
+    }                                                   // clockwise
+    if(LSB == LOW && halfright == true){          // -->
+      halfright = false;                                // One whole click clockwise
+      rotaryNumber++;
+    }
+  }
+}
